b1baec01145a511bf5b8de5330c8a28e0d74564d
JNickBaughan <JNickBaughan@gmail.com>
2021-02-17 09:01:31 -0500

    fix unit tests

--- test/helpers/index.test.js
+++ test/helpers/index.test.js
@@ -18,98 +18,73 @@ test("sorts todos", () => {
 test("sorts todos", () => {
   const todos = [
     {
-      id: 1,
-      todo: "Clean attic",
-      complete: false
+      id: 1
     },
     {
       id: 2,
-      todo: "add shelves in attic",
-      complete: false,
       parent: 1
     },
     {
       id: 3,
-      todo: "add shelves beside attic vent",
-      complete: false,
       parent: 2
     },
     {
       id: 4,
-      todo: "add shelves by attic door",
-      complete: false,
       parent: 2
     },
     {
       id: 5,
-      todo: "move old clothes",
-      complete: false,
       parent: 1
     },
     {
-      id: 6,
-      todo: "finish Kitchen renovations",
-      complete: false
+      id: 6
     },
     {
       id: 7,
-      todo: "tile backsplash",
-      complete: false,
       parent: 6
     },
     {
       id: 8,
-      todo: "paint wall",
-      complete: false,
       parent: 6
     }
   ];
   const expected = [
     {
       id: 1,
-      todo: "Clean attic",
-      complete: false,
       children: [
         {
           id: 2,
-          todo: "add shelves in attic",
-          complete: false,
           parent: 1,
           children: [
             {
               id: 3,
-              todo: "add shelves beside attic vent",
-              complete: false,
               parent: 2,
               children: []
             },
             {
               id: 4,
-              todo: "add shelves by attic door",
-              complete: false,
               parent: 2,
               children: []
             }
           ]
+        },
+        {
+          id: 5,
+          parent: 1,
+          children: []
         }
       ]
     },
     {
       id: 6,
-      todo: "finish Kitchen renovations",
-      complete: false,
       children: [
         {
           id: 7,
-          todo: "tile backsplash",
-          complete: false,
           parent: 6,
           children: []
         },
         {
           id: 8,
-          todo: "paint wall",
-          complete: false,
           parent: 6,
           children: []
         }
