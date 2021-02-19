fb80f5bc372055a57b2b6308e98426ebbb07b78a
JNickBaughan <JNickBaughan@gmail.com>
2021-02-18 21:44:58 -0500

    connection test

--- src/server/pool.js
+++ src/server/pool.js
@@ -5,7 +5,8 @@ class Pool {
 
   connect(options) {
     this._pool = new pg.Pool(options);
-    return this._pool.query("SELECT * FROM todos.todo");
+    //return this._pool.query("SELECT * FROM todos.todo");
+    return this._pool.query("SELECT 1 + 1");
   }
 }
 
