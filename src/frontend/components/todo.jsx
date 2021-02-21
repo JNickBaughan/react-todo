9798561b1dd4a8a2da4d9aef1da0aef1b29024e1
JNickBaughan <JNickBaughan@gmail.com>
2021-02-21 11:07:30 -0500

    add checkbox functionality

--- src/frontend/components/todo.jsx
+++ src/frontend/components/todo.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-export const Todo = ({ todo, complete, marginLeft }) => {
+export const Todo = ({ todo, complete, marginLeft, id, checkTodo }) => {
   return (
     <label class="container" style={{ marginLeft: `${marginLeft}` }}>
-      <input type="checkbox" checked={complete ? "checked" : ""} />
+      <input
+        type="checkbox"
+        checked={complete ? "checked" : ""}
+        onClick={() => {
+          checkTodo(id);
+        }}
+      />
       <span class="checkmark"></span>
       {todo}
+      <span class="plus alt"></span>
     </label>
   );
 };
+
+export default Todo;
--- src/server/server.js
+++ src/server/server.js
@@ -34,73 +34,97 @@ Pool.connect(dbConfig).then(() => {
                         html,body{ width: 98vw; height: 96vh; font-family: Verdana, sans-serif;  }
                         #root{ height: 100%; }
                         
-/* The container */
-.container {
-  display: block;
-  position: relative;
-  padding-left: 35px;
-  margin-bottom: 12px;
-  cursor: pointer;
-  font-size: 22px;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-}
+                        /* The container */
+                        .container {
+                          display: block;
+                          position: relative;
+                          padding-left: 35px;
+                          margin-bottom: 12px;
+                          cursor: pointer;
+                          font-size: 22px;
+                          -webkit-user-select: none;
+                          -moz-user-select: none;
+                          -ms-user-select: none;
+                          user-select: none;
+                        }
 
-/* Hide the browser's default checkbox */
-.container input {
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
-  height: 0;
-  width: 0;
-}
+                        /* Hide the browser's default checkbox */
+                        .container input {
+                          position: absolute;
+                          opacity: 0;
+                          cursor: pointer;
+                          height: 0;
+                          width: 0;
+                        }
 
-/* Create a custom checkbox */
-.checkmark {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 25px;
-  width: 25px;
-  background-color: #eee;
-}
+                        /* Create a custom checkbox */
+                        .checkmark {
+                          position: absolute;
+                          top: 0;
+                          left: 0;
+                          height: 25px;
+                          width: 25px;
+                          background-color: #eee;
+                        }
 
-/* On mouse-over, add a grey background color */
-.container:hover input ~ .checkmark {
-  background-color: #ccc;
-}
+                        /* On mouse-over, add a grey background color */
+                        .container:hover input ~ .checkmark {
+                          background-color: #ccc;
+                        }
 
-/* When the checkbox is checked, add a blue background */
-.container input:checked ~ .checkmark {
-  background-color: #2196F3;
-}
+                        /* When the checkbox is checked, add a blue background */
+                        .container input:checked ~ .checkmark {
+                          background-color: #2196F3;
+                        }
 
-/* Create the checkmark/indicator (hidden when not checked) */
-.checkmark:after {
-  content: "";
-  position: absolute;
-  display: none;
-}
+                        /* Create the checkmark/indicator (hidden when not checked) */
+                        .checkmark:after {
+                          content: "";
+                          position: absolute;
+                          display: none;
+                        }
 
-/* Show the checkmark when checked */
-.container input:checked ~ .checkmark:after {
-  display: block;
-}
+                        /* Show the checkmark when checked */
+                        .container input:checked ~ .checkmark:after {
+                          display: block;
+                        }
 
-/* Style the checkmark/indicator */
-.container .checkmark:after {
-  left: 9px;
-  top: 5px;
-  width: 5px;
-  height: 10px;
-  border: solid white;
-  border-width: 0 3px 3px 0;
-  -webkit-transform: rotate(45deg);
-  -ms-transform: rotate(45deg);
-  transform: rotate(45deg);
-}
+                        /* Style the checkmark/indicator */
+                        .container .checkmark:after {
+                          left: 9px;
+                          top: 5px;
+                          width: 5px;
+                          height: 10px;
+                          border: solid white;
+                          border-width: 0 3px 3px 0;
+                          -webkit-transform: rotate(45deg);
+                          -ms-transform: rotate(45deg);
+                          transform: rotate(45deg);
+                        }
+
+                        .plus {
+                          display:inline-block;
+                          width:50px;
+                          height:50px;
+                          position: absolute;
+                            bottom: -9px;
+                          background:
+                            linear-gradient(#fff,#fff),
+                            linear-gradient(#fff,#fff),
+                            #000;
+                          background-position:center;
+                          background-size: 50% 2px,2px 50%; /*thickness = 2px, length = 50% (25px)*/
+                          background-repeat:no-repeat;
+                        }
+
+                        .alt {
+                          background:
+                            linear-gradient(#000,#000),
+                            linear-gradient(#000,#000);
+                          background-position:center;
+                          background-size: 50% 2px,2px 50%; /*thickness = 2px, length = 50% (25px)*/
+                          background-repeat:no-repeat;
+                        }
                         </style>
                         <html>
                           <head></head>
